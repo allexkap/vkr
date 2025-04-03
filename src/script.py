f69394e2d91ad444d1a8abdb62ef8e0dd700c150
allexkap <aekpstn@gmail.com>
2025-04-03 18:13:39 +0300

    add more args

--- src/script.py
+++ src/script.py
@@ -1,18 +1,27 @@
 import argparse
 import logging
 import os
-import sys
+import shlex
 from pathlib import Path
-
-import mount
+from shutil import rmtree
 
 RUN_DIRS = ['wayland-1', 'pipewire-0', 'pulse']
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s %(filename)s:%(lineno)d %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--path', type=Path, default='/var/tmp/vkr')
     parser.add_argument('-n', '--name', type=str, default='')
+    parser.add_argument('-r', '--remove', action='store_true')
+    parser.add_argument('-p', '--private', action='store_true')
+    parser.add_argument('-c', '--current-dir', action='store_true')
+    parser.add_argument('--path', type=Path, default='~/.local/state/vkr')
+    parser.add_argument('--bwrap-args', type=str)
     parser.add_argument('inner', nargs=argparse.REMAINDER)
 
     args = parser.parse_args()
@@ -20,7 +29,7 @@ def parse_args():
         args.inner = ['bash']
     if args.name == '':
         args.name = Path(args.inner[0]).name
-    args.path /= args.name
+    args.path = args.path.expanduser() / args.name
     return args
 
 
@@ -43,6 +52,10 @@ def print_res_ug_id():
 def main():
     args = parse_args()
 
+    if args.remove:
+        rmtree(args.path)
+        exit(0)
+
     if not args.path.exists():
         args.path.mkdir(parents=True)
 
@@ -52,15 +65,22 @@ def main():
     ]
 
     # fmt:off
-    special_fs = [
+    special_dirs = [
         '--ro-bind', '/', '/',
         '--proc', '/proc',
         '--dev', '/dev',
     ]
-    home_fs = [
-        '--bind', args.path.as_posix(), '/home',
-    ]
     # fmt:on
+
+    home_dirs = []
+    if args.private:
+        home_dirs.extend(['--tmpfs', os.environ['HOME']])
+    else:
+        home_dirs.extend(['--bind', args.path.as_posix(), os.environ['HOME']])
+
+    other_dirs = []
+    if args.current_dir:
+        home_dirs.extend(['--bind', os.getcwd(), os.getcwd()])
 
     xdg_runtime_dir = os.environ['XDG_RUNTIME_DIR']
     run_dirs = ['--tmpfs', '/run']
@@ -68,16 +88,22 @@ def main():
         d = f'{xdg_runtime_dir}/{d}'
         run_dirs.extend(['--bind', d, d])
 
+    passthrough_args = []
+    if args.bwrap_args is not None:
+        passthrough_args.extend(shlex.split(args.bwrap_args))
+
     bwrap_args = [
         'bwrap',
         *flags,
-        *special_fs,
-        *home_fs,
+        *special_dirs,
+        *home_dirs,
         *run_dirs,
+        *other_dirs,
+        *passthrough_args,
         *args.inner,
     ]
 
-    print(' '.join(bwrap_args))
+    logging.debug(' '.join(bwrap_args))
     os.execvp(bwrap_args[0], bwrap_args)
 
 
